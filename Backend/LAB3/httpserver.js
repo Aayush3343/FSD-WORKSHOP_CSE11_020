d6a766d20dacf84f4def2c7e550d16c4d89ea6fb
Aayush3343 <aayush.25b01010403@abes.ac.in>
2026-08-21 14:21:24 +0530

    Assignment 1

---
+++ Backend/LAB3/httpserver.js
@@ -0,0 +1,8 @@
+import http, { createServer } from "http";
+
+const server = http.createServer((req, res) => {
+
+})
+server.listen(3000, () =>{
+console.log("Server is running in port number 3000")
+})
