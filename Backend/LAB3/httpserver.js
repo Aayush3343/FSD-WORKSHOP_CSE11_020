8cee06770adcbed691323385a117eac1b22f55b2
Aayush3343 <aayush.25b01010403@abes.ac.in>
2026-09-03 16:07:21 +0530

    03 September 2026

--- Backend/LAB3/httpserver.js
+++ Backend/LAB3/httpserver.js
@@ -4,24 +4,28 @@ const port = 3000;
 
 const users = [
     {
+        id: 1,
         name: "Aayush Sharma",
         phone: "9968888400",
-        email: "aayush@gmail.com"
+        email: "aastik@gmail.com"
     },
     {
-        name: "Aayush Sharma",
-        phone: "9968888400",
-        email: "aayush@gmail.com"
+        id: 2,
+        name: "Aastik Rai",
+        phone: "7827536087",
+        email: "aastik@gmail.com"
     },
     {
-        name: "Aayush Sharma",
-        phone: "9968888400",
-        email: "aasyush@gmail.com"
+        id: 3,
+        name: "Abhay Singh",
+        phone: "919291929192",
+        email: "adalla@gmail.com"
     },
     {
-        name: "Aayush Sharma",
-        phone: "9968888400",
-        email: "aayush@gmail.com"
+        id: 4,
+        name: "Prashant music",
+        phone: "6767575747474",
+        email: "prashant@gmail.com"
     }
 ];
 
@@ -30,7 +34,6 @@ const server = http.createServer((req, res) => {
     const url = req.url;
     const method = req.method;
 
-    
     if (url === "/msg" && method === "GET") {
 
         res.statusCode = 200;
@@ -39,7 +42,6 @@ const server = http.createServer((req, res) => {
         res.end("Hello World");
     }
 
-  
     else if (url === "/sys" && method === "GET") {
 
         res.statusCode = 404;
@@ -48,7 +50,7 @@ const server = http.createServer((req, res) => {
         res.end("Page not found");
     }
 
-   
+    // GET all users
     else if (url === "/users" && method === "GET") {
 
         res.statusCode = 200;
@@ -57,7 +59,24 @@ const server = http.createServer((req, res) => {
         res.end(JSON.stringify(users));
     }
 
-    
+    // GET user by id
+    else if (url.startsWith("/users/") && method === "GET") {
+
+        const id = url.split("/")[2];
+
+        const user = users.find((u) => u.id == id);
+
+        if (!user) {
+            return res.end("User not found");
+        }
+
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+
+        res.end(JSON.stringify(user));
+    }
+
+    // CREATE user
     else if (url === "/create" && method === "POST") {
 
         let body = "";
@@ -65,26 +84,22 @@ const server = http.createServer((req, res) => {
         req.on("data", (content) => {
             body = body + content;
         });
-        const newUser = JSON.parse(body);
-        users.push(newUser);
-        res.write(JSON.stringify({
-                    user:newUser
-        }));
-        
 
-               
+        req.on("end", () => {
 
+            const newUser = JSON.parse(body);
 
-        
-        
+            users.push(newUser);
 
-                
-               
+            res.statusCode = 201;
+            res.setHeader("Content-Type", "application/json");
 
-           
+            res.end(JSON.stringify({
+                user: newUser
+            }));
+        });
     }
 
-   
     else {
 
         res.statusCode = 404;
