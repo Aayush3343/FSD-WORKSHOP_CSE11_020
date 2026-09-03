bf42833551e4ee274f9eaf0f9316b85f119bf35b
Aayush3343 <aayush.25b01010403@abes.ac.in>
2026-09-03 10:22:52 +0530

    27 AUG 2026

--- Backend/LAB3/httpserver.js
+++ Backend/LAB3/httpserver.js
@@ -1,8 +1,99 @@
-import http, { createServer } from "http";
+import http from 'http';
+
+const port = 3000;
+
+const users = [
+    {
+        name: "Aayush Sharma",
+        phone: "9968888400",
+        email: "aayush@gmail.com"
+    },
+    {
+        name: "Aayush Sharma",
+        phone: "9968888400",
+        email: "aayush@gmail.com"
+    },
+    {
+        name: "Aayush Sharma",
+        phone: "9968888400",
+        email: "aasyush@gmail.com"
+    },
+    {
+        name: "Aayush Sharma",
+        phone: "9968888400",
+        email: "aayush@gmail.com"
+    }
+];
 
 const server = http.createServer((req, res) => {
 
-})
-server.listen(3000, () =>{
-console.log("Server is running in port number 3000")
-})
+    const url = req.url;
+    const method = req.method;
+
+    
+    if (url === "/msg" && method === "GET") {
+
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/plain");
+
+        res.end("Hello World");
+    }
+
+  
+    else if (url === "/sys" && method === "GET") {
+
+        res.statusCode = 404;
+        res.setHeader("Content-Type", "text/plain");
+
+        res.end("Page not found");
+    }
+
+   
+    else if (url === "/users" && method === "GET") {
+
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+
+        res.end(JSON.stringify(users));
+    }
+
+    
+    else if (url === "/create" && method === "POST") {
+
+        let body = "";
+
+        req.on("data", (content) => {
+            body = body + content;
+        });
+        const newUser = JSON.parse(body);
+        users.push(newUser);
+        res.write(JSON.stringify({
+                    user:newUser
+        }));
+        
+
+               
+
+
+        
+        
+
+                
+               
+
+           
+    }
+
+   
+    else {
+
+        res.statusCode = 404;
+        res.setHeader("Content-Type", "text/plain");
+
+        res.end("Route not found");
+    }
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
